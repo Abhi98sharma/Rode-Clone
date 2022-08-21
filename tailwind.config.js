eec906c47a7135785911d0f7c470a70fcf0bcb6b
Abhijeet98s <abhi.21smiley00@gmail.com>
2022-08-22 04:49:17 +0530

    fixed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,9 @@ module.exports = {
     extend: {
       colors: {
         gold: "#ad9970",
-        lightGray: "#687583",
-        garyShade: "#767676",
-        footer: "#394a58",
-        blackShade: "#262626",
+      },
+      fontFamily: {
+        inter: ["Inter", "san-serif"],
       },
     },
   },
